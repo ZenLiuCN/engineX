fd349f2a9e41547e72715521ba8d0c52c835f6d2
Zen.Liu <lcz20@163.com>
2025-03-09 21:55:53 +0800

    merge to 0.6.8

--- engine/build_win.sh
+++ engine/build_win.sh
@@ -5,4 +5,4 @@ go build -tags=duckdb_use_lib,all -o engine.exe \
 -ldflags="-X main.Version=$(Git describe --abbrev=0 --tags) -s -w" . && \
 # upx to compress the executable.
 upx --lzma engine.exe
-# HTTP_PROXY=127.0.0.1:1079 GOPROXY=https://proxy.golang.org GO111MODULE=on  go get github.com/ZenLiuCN/engine@v0.6.4
+# HTTP_PROXY=127.0.0.1:1079 GOPROXY=https://proxy.golang.org GO111MODULE=on  go get github.com/ZenLiuCN/engine@v0.6.8
